9c7270545c38ce863c01b75b232e3c0ed8b43c85
simoncoelho <36525527+simoncoelho@users.noreply.github.com>
2022-12-06 20:46:01 -0600

    Change bullets

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-function customNav(){
-    return (
+const customNav =
         <nav>
             <h1>scoelho.io</h1>
             <ul>
-                <li>Pricing</li>
-                <li>About</li>
-                <li>Contact</li>
+                <li>.jpeg</li>
+                <li>.toe</li>
+                <li>.git</li>
+                <li>.me</li>
             </ul>
         </nav>
-    );
-}
 
 ReactDOM.render(
-    customNav(), 
+    customNav, 
     document.getElementById('root'));
