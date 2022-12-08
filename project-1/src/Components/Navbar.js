448b3e4d317c722394095be1e9299420dbeb162a
simoncoelho <36525527+simoncoelho@users.noreply.github.com>
2022-12-07 21:34:35 -0600

    Get basics of the site in

---
+++ project-1/src/Components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function NavBar () {
+    return (
+        <nav>
+            <div className="logoBox">
+\                <h1>scoelho.io</h1>            
+            </div>
+            <ul>
+                <li>.jpeg</li>
+                <li>.toe</li>
+                <li>.git</li>
+                <li>.me</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
