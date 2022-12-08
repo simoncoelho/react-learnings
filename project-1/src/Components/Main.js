052684e123b1451a11043a646f99b21026ef0b3b
simoncoelho <36525527+simoncoelho@users.noreply.github.com>
2022-12-07 21:34:49 -0600

    Finish basic changes commit

---
+++ project-1/src/Components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Main () {
+    return (
+        <div>
+            <p>Content for my site would theoretically go here.</p>
+        </div>
+    )
+}
+
+
+export default Main;
